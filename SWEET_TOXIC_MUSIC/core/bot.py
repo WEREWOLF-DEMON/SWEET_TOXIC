4fdf653750e5215dfd02b43a6d86a3152b8f6a69
SWEET-TOXIC-DEVIL <153902241+SWEET-TOXIC-DEVIL@users.noreply.github.com>
2024-02-22 19:46:27 +0530

    bot.py

--- SWEET_TOXIC_MUSIC/core/bot.py
+++ SWEET_TOXIC_MUSIC/core/bot.py
@@ -6,7 +6,7 @@ import config
 from ..logging import LOGGER
 
 
-class SWEET_TOXIC_MUSIC(Client):
+class SWEET_TOXIC_DEVIL(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
