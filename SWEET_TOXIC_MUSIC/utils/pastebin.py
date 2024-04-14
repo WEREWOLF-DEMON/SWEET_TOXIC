cc8643d262e063e9094d09f560d981cd519bce86
WEREWOLF-DEMON <153902241+WEREWOLF-DEMON@users.noreply.github.com>
2024-04-14 13:49:09 +0530

    pastebin.py

--- SWEET_TOXIC_MUSIC/utils/pastebin.py
+++ SWEET_TOXIC_MUSIC/utils/pastebin.py
@@ -39,7 +39,7 @@ async def post(url: str, *args, **kwargs):
         return data
 
 
-async def SWEET_TOXIC_DEV8LBin(text):
+async def SWEET_TOXIC_DEVILBin(text):
     resp = await post(f"{BASE}api/v2/paste", data=text)
     if not resp["success"]:
         return
